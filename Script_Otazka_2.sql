6ea60ed8920b879104816517a965f623ab6b954c
DanRizak <122486651+DanRizak@users.noreply.github.com>
2024-07-12 02:39:26 +0200

    Update Script_Otazka_2.sql
    commands formatting

--- Script_Otazka_2.sql
+++ Script_Otazka_2.sql
@@ -9,11 +9,16 @@ SELECT fin1.`year`, fin1.value_current_year AS avg_wage_sector, fin1.category_na
 FROM (
 	SELECT `year`, value_current_year, category_name    -- prumerne vyplaty ve 2006 a 2018 podle odvetvi
 	FROM t_daniel_rizak_project_sql_primary_final 
-	WHERE unit = 'czk' AND `year` IN (2006, 2018)) fin1
+	WHERE unit = 'czk'
+		AND `year` IN (2006, 2018)
+) fin1
 JOIN (
 	SELECT `year`, value_current_year, category_name, region_name, category_code   -- prumerne ceny chleba ve 2006 a 2018 podle kraju
 	FROM t_daniel_rizak_project_sql_primary_final
-	WHERE unit != 'czk' AND `year` IN (2006, 2018) AND category_code = '111301') fin2
+	WHERE unit != 'czk' 
+		AND `year` IN (2006, 2018) 
+		AND category_code = '111301'
+) fin2
 	ON fin1.`year` = fin2.`year`;
 
 -- pridame klauzuli Order By
@@ -23,11 +28,15 @@ SELECT fin1.`year`, fin1.value_current_year AS avg_wage_sector, fin1.category_na
 FROM (
 	SELECT `year`, value_current_year, category_name    -- prumerne vyplaty ve 2006 a 2018 podle odvetvi
 	FROM t_daniel_rizak_project_sql_primary_final 
-	WHERE unit = 'czk' AND `year` IN (2006, 2018)) fin1
+	WHERE unit = 'czk' 
+		AND `year` IN (2006, 2018)) fin1
 JOIN (
 	SELECT `year`, value_current_year, category_name, region_name, category_code   -- prumerne ceny chleba ve 2006 a 2018 podle kraju
 	FROM t_daniel_rizak_project_sql_primary_final
-	WHERE unit != 'czk' AND `year` IN (2006, 2018) AND category_code = '111301') fin2
+	WHERE unit != 'czk'
+		AND `year` IN (2006, 2018)
+		AND category_code = '111301'
+) fin2
 	ON fin1.`year` = fin2.`year`
 	ORDER BY bread_kg_for_wage
 
@@ -39,13 +48,15 @@ SELECT fin1.`year`, fin1.value_current_year AS avg_wage_sector, fin1.category_na
 FROM (
 	SELECT `year`, value_current_year, category_name  -- prumerne vyplaty ve 2006 a 2018 podle odvetvi
 	FROM t_daniel_rizak_project_sql_primary_final 
-	WHERE unit = 'czk' AND `year` IN (2006, 2018)) fin1
+	WHERE unit = 'czk' 
+	AND `year` IN (2006, 2018)
+) fin1
 JOIN (
 	SELECT `year`, value_current_year, category_name, region_name, category_code   -- prumerne ceny mleka ve 2006 a 2018 podle kraju
 	FROM t_daniel_rizak_project_sql_primary_final
-	WHERE unit != 'czk' AND `year` IN (2006, 2018) AND category_code = '114201') fin2
+	WHERE unit != 'czk' 
+		AND `year` IN (2006, 2018) 
+		AND category_code = '114201'
+) fin2
 	ON fin1.`year` = fin2.`year`
-ORDER BY milk_litres_for_wage;
-
-
-
+	ORDER BY milk_litres_for_wage;
